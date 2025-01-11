1e8e3a15b4e5794bbdacc73da710d5c80503a3d5
yamilobt8 <yamilo12345678910@gmail.com>
2025-01-11 02:08:00 +0100

    user checking

--- card_generator/models.py
+++ card_generator/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Participant(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    insta_url = models.URLField()
+    dance_category = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    picutre_url = models.URLField()
+    def __str__(self):
+        return self.full_name
--- card_generator/views.py
+++ card_generator/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Participant
 # Create your views here.
 def index(request):
     return render(request, 'card_generator/home.html')
+
+def generate(request):
+    if request.method == 'POST':
+        full_name = clean_input(request.POST.get('full_name'))
+        phone_number = clean_input(request.POST.get('phone_number'))
+        dance_category = clean_input(request.POST.get('dance_category'))
+        age = clean_input(request.POST.get('age'))
+
+        if Participant.objects.filter(full_name=full_name).exists() or Participant.objects.filter(phone_number=phone_number).exists():
+            user = Participant.objects.filter(full_name=full_name).first()
+            return render(request, 'card_generator/card.html', {'user': user})
+
+        # Redirect to a success page or render a template
+        return render(request, 'card_generator/home.html')  # replace with the name of your success URL
+    return render(request, 'card_generator/home.html')  # Render the form template
+
+def clean_input(value):
+    if value:
+        return value.lower().replace(' ', '')
+    return value
